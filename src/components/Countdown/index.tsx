85e3cf77c3bc692a7a885e96d37de7f6d01773e0
pmqueiroz <pedroqueiroz593@gmail.com>
2021-02-23 12:09:10 -0300

    countdown half done

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,21 +1,45 @@
+import { useState, useEffect } from 'react';
 import { Container, ButtonContainer } from './styles';
 
 export default function Coutdown() {
+   const [time, setTime] = useState(25 * 60);
+   const [active, setActive] = useState(false);
+
+   const minutes = Math.floor(time / 60);
+   const seconds = time % 60;
+
+   const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+   useEffect(() => {
+      if(active && time > 0) {
+         setTimeout(() => {
+            setTime(time -1);
+         }, 1000)
+      }
+
+   }, [active, time])
+
+   function handleStartCountdown() {
+      setActive(!active)
+   }
+
    return (
       <>
          <Container>
             <div>
-               <span>2</span>
-               <span>5</span>
+               <span>{minuteLeft}</span>
+               <span>{minuteRight}</span>
             </div>
                <span>:</span>
             <div>
-               <span>0</span>
-               <span>0</span>
+               <span>{secondLeft}</span>
+               <span>{secondRight}</span>
             </div>
          </Container>
-         <ButtonContainer type="button">
-            Inciar um ciclo
+         <ButtonContainer type="button" onClick={handleStartCountdown}>
+            {active ? 'Stop' : 'Start'} 
+            &nbsp; cicle
          </ButtonContainer>
       </>
    );
