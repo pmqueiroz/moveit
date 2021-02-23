f73eb76655d69f718d90b90ee45c90bd116545ee
pmqueiroz <pedroqueiroz593@gmail.com>
2021-02-23 11:53:53 -0300

    fix: container height

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-   height: 100vh;
+   height: calc(100vh - 5rem);
    max-width: 992px;
    margin: 0 auto;
    padding: 2.5rem 2rem;
